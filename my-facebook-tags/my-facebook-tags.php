5b265d25fd5f678a90b83a8c4a38e1e75ad33d86
vinayak9110 <thoratvinayak99@gmail.com>
2017-11-15 15:23:54 +0530

    Add files via upload

---
+++ my-facebook-tags/my-facebook-tags.php
@@ -0,0 +1,11 @@
+<?php
+	/**
+ * Plugin Name: My Facebook Tags
+ * Plugin URI: http://danielpataki.com
+ * Description: This plugin adds some Facebook Open Graph tags to our single posts.
+ * Version: 1.0.0
+ * Author: Vinayak Thorat 
+ * Author URI: thoratvinayak99@gmail.com
+ * License: GPL2
+ */
+?>
